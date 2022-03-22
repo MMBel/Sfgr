653ab3bc9abc1f526417d843bf0fd6af3d7d3ae9
MMBel <mih.m.belovolov@gmail.com>
2022-03-22 17:42:16 +0300

    Исправлено удаление кадров

--- main.cpp
+++ main.cpp
@@ -5,39 +5,39 @@ using namespace std;
 
 int main()
 {
-    double pre2PI = 2.d*M_PI;
     unsigned Amplitude = 25;
-
 
     Sfgr graph;
     graph.cfg.ScrshotDir="a:\\CPP\\Sfgr";
     graph.cfg.Title=L"Пример синусоидного графика";
-    graph.cfg.WinHeightPercent=90;
-    graph.cfg.WinWidthPercent=90;
+    graph.cfg.WinHeightPercent=50;
+    graph.cfg.WinWidthPercent=50;
+    graph.cfg.LMargin = 80;
+    graph.cfg.RMargin = 30;
+    graph.cfg.TMargin = 60;
+    graph.cfg.BMargin = 60;
     graph.Start();
     int GraphSize = graph.cfg.XDimMax-graph.cfg.XDimMin;
 
+    Sfgr::Frame* z;
     Sfgr::Frame* d = graph.NewFrame(GraphSize);
     Sfgr::Frame* f = graph.NewFrame(GraphSize);
+
 
     graph.ShowFrame(d);
     graph.ShowFrame(f);
 
-    d->GraphColor=sf::Color::Blue;
-    f->GraphColor=sf::Color::Magenta;
-
     graph.AddPoint(f, 20, -5, 3, sf::Color::Red, "Point description", 10);
     graph.AddVLine(f, 1500, 1, sf::Color::Red, "1500", 12);
     graph.AddHLine(f, -30, 1, sf::Color::Red, "center", 12);
-    graph.AddHText(f, graph.cfg.Vidmode.width, 20, "GS=" + std::to_string(GraphSize), 16, sf::Color::Blue, sf::Text::Regular, Sfgr::TextPos::LEFTER);
 
 
-    for(int a=500; a>5; a--)
+    for(int a=3000; a>1; a--)
     {
-        double SineLength = 1.d*200000/a;
+        z = graph.NewFrame(GraphSize);
+        graph.AddHText(z, graph.cfg.Vidmode.width, 20, "A=" + std::to_string(a), 16, sf::Color::Blue, sf::Text::Regular, Sfgr::TextPos::LEFTER);
 
-        graph.HideFrame(f);
-        graph.HideFrame(d);
+        double SineLength = 1.d*200000/a;
 
         d->ArrayGraph.clear();
         f->ArrayGraph.clear();
@@ -46,20 +46,23 @@ int main()
         {
             Sfgr::point pnt1;
             Sfgr::point pnt2;
-            pnt1.XValue=pnt2.XValue=10+i;
 
+            float SineVal = 1.f*Amplitude*sin(i*2.f*M_PI/SineLength);
 
-            pnt1.YValue=float(-30+Amplitude*sin(i*pre2PI/SineLength));
+            pnt1.XValue = 10+i;
+            pnt2.XValue = pnt1.XValue;
+
+            pnt1.YValue=-30+SineVal;
+            pnt2.YValue=-30-SineVal;
+
             d->ArrayGraph.push_back(pnt1);
-
-            pnt2.YValue=float(-30-Amplitude*sin(i*pre2PI/SineLength));
             f->ArrayGraph.push_back(pnt2);
+
         }
+        graph.ShowFrame(z);
+        sf::sleep(sf::milliseconds(5));
+        graph.DeleteFrame(z);
 
-        graph.ShowFrame(d);
-        graph.ShowFrame(f);
-
-        sf::sleep(sf::milliseconds(50));
     }
     cout << "Done" << endl;
     cin.ignore();
